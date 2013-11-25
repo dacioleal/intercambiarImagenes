470f4a8ba2ba7c26215db39779b6169e1743b888
dacioleal <dacioleal@gmail.com>
2013-11-25 11:06:23 +0100

    Initial Commit

---
+++ pruebaLayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  pruebaLayer
+//
+//  Created by Dacio Leal Rodriguez on 25/11/13.
+//  Copyright (c) 2013 Dacio Leal Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ pruebaLayerTests/pruebaLayerTests.m
@@ -0,0 +1,34 @@
+//
+//  pruebaLayerTests.m
+//  pruebaLayerTests
+//
+//  Created by Dacio Leal Rodriguez on 25/11/13.
+//  Copyright (c) 2013 Dacio Leal Rodriguez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface pruebaLayerTests : XCTestCase
+
+@end
+
+@implementation pruebaLayerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
